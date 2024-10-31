118fa3daf7ff9138b39403910f4743ca81baa780
Ali Shirmohammadi <a.shirmohammadi@nta.local>
2024-10-31 12:27:08 +0330

    write authentication test

--- src/test/java/ir/selab/tdd/AuthenticationServiceTest.java
+++ src/test/java/ir/selab/tdd/AuthenticationServiceTest.java
@@ -3,11 +3,18 @@ package ir.selab.tdd;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class AuthenticationServiceTest {
     @Test
     public void userCreationTest() {
         User ali = new User("ali", "1234");
         assertEquals("ali", ali.getUsername());
+    }
+
+    @Test
+    public void userServiceAuthenticationTest() {
+        UserService userService = new UserService();
+        assertFalse(userService.authenticate("1", "2"));
     }
 }
